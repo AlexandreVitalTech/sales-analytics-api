765b2de7abc57ad028b731e6c5cf307a229098da
Alexandre Vital <alexandre2016vital@gmail.com>
2026-03-18 21:41:30 -0300

    Começo da API

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-from reader import carregar_venda
+from fastapi import FastAPI
+from src.reader import carregar_venda
+from src.reports import sintetizar_relatorio
+app = FastAPI()
 dados = carregar_venda()
+@app.get("/")
+def root():
+    return {"mensagem": "Olá, fastAPI!"}
+"""
+@app.get("/categoria")
+def categoria():
+    return {}
+"""
